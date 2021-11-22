2c7ea0379ea4a565d8cdb4101cac139919de4cbd
Nick Hecht <chilliwebs@gmail.com>
2021-11-22 03:10:46 +0000

    test

--- service.py
+++ service.py
@@ -78,6 +78,7 @@ def tree():
                         else:
                             strip.setPixelColor(n*2, Color(0, 0, 0))
                             strip.setPixelColor((n*2)+1, c)
+                    sleep(0.1)
                     strip.show()
         
         if mode == 5:
