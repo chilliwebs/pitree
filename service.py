f0406bf50a4e23ae78387771f1a4c1619cf82819
chillidogg7 <chilliwebs@gmail.com>
2022-11-17 12:11:27 -0500

    test

--- service.py
+++ service.py
@@ -221,7 +221,7 @@ def worker():
                 build_expr(x_expr, y_expr)
             if item == 56: # chase
                 x_expr = "(i + (t * s)) / c"
-                y_expr = "sin(sin(i) + (t * s)) + (3.1415/4)"
+                y_expr = "2 * sin((2 * i) + (t * s)) - 1"
                 build_expr(x_expr, y_expr)
 
 
