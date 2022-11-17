75739e450f8aef0bac5c00885836afd809cddd87
chillidogg7 <chilliwebs@gmail.com>
2022-11-17 11:49:28 -0500

    move sin

--- service.py
+++ service.py
@@ -217,7 +217,7 @@ def worker():
             
             if item == 55: # chase
                 x_expr = "i + (t * s)"
-                y_expr = "sin(i)"
+                y_expr = "sin(i + (t * s))"
                 build_expr(x_expr, y_expr)
 
 
