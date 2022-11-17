092e943a2d297d8ac7ea149db9e8f7afabdb13cd
chillidogg7 <chilliwebs@gmail.com>
2022-11-16 21:29:47 -0500

    full tilt

--- service.py
+++ service.py
@@ -67,7 +67,6 @@ def shutdown_server():
 # led buffer
 BUFF = [Color(0, 0, 0)] * LED_COUNT
 
-AVG_SLEEP = 0.0
 PALATE = WARM_WHITE
 SPEED = 1
 
@@ -78,7 +77,6 @@ def tree():
     global run
     while run:
         # global mode
-        global AVG_SLEEP
         global PALATE
         global SPEED
 
@@ -97,9 +95,6 @@ def tree():
             strip.setPixelColor(i, BUFF[i])
         
         strip.show()
-        slp = max(0.05-(time.time()-t), 0)
-        AVG_SLEEP = (AVG_SLEEP + slp)/2.0
-        time.sleep(slp)
 
         # # Warm White
         # if mode == 0:
@@ -253,11 +248,6 @@ def bg_img():
 @app.route("/ver")
 def ver():
     return '0.0.30'
-
-@app.route("/AVG_SLEEP")
-def avg_slp():
-    global AVG_SLEEP
-    return str(AVG_SLEEP)
 
 @app.route("/hasupdate")
 def hasupdate():
