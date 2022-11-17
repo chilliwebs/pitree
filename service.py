045e71f5e3b4aeacadf24489335921d047379ed7
chillidogg7 <chilliwebs@gmail.com>
2022-11-17 09:28:56 -0500

    test

--- service.py
+++ service.py
@@ -239,7 +239,7 @@ def worker():
 
             if item == 12: # wipe
                 SPEED = 10
-                x_expr = "i // 50"
+                x_expr = "t // 50"
                 y_expr = "i * 0"
                 build_expr(x_expr, y_expr)
 
