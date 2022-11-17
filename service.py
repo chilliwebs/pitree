3cbeff0b6d0c50d324d4f77e61edfa48e4bf539b
chillidogg7 <chilliwebs@gmail.com>
2022-11-16 19:15:42 -0500

    fix random

--- service.py
+++ service.py
@@ -48,9 +48,9 @@ BRIGHT_WHITE = [Color(255, 255, 255)]
 WARM_WHITE = [Color(255, 175, 75)]
 RGBYAV = [Color(255, 0, 0), Color(0, 255, 0), Color(0, 0, 255), Color(255, 255, 0), Color(0, 255, 255), Color(255, 0, 255)]
 
-RANDOM = [Color(0, 0, 0)] * LED_COUNT
+RANDOM = [[Color(0, 0, 0)]] * LED_COUNT
 for i in range(0, LED_COUNT):
-    RANDOM[i] = Color(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    RANDOM[i] = [Color(random.randint(0,255), random.randint(0,255), random.randint(0,255))]
 
 RAINBOW = [[Color(0, 0, 0)]] * LED_COUNT
 for j in range(0, 255, 1):
