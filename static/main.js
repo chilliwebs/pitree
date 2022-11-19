9aca9da6b0de0a769c26af30fb3068f350338cea
chillidogg7 <chilliwebs@gmail.com>
2022-11-18 20:19:10 -0500

    fix

--- static/main.js
+++ static/main.js
@@ -50,53 +50,53 @@ export function onload() {
         })(btn);
     }
 
-    document.getElementById('addColor').addEventListener('click', function (event) {
-        var controll = document.createElement('div');
-        controll.setAttribute('id', 'colorControll' + colorIncrement);
+    // document.getElementById('addColor').addEventListener('click', function (event) {
+    //     var controll = document.createElement('div');
+    //     controll.setAttribute('id', 'colorControll' + colorIncrement);
 
-        var slider = document.createElement('input');
-        slider.setAttribute('type', 'range');
-        slider.setAttribute('min', '0');
-        slider.setAttribute('max', '800');
-        slider.setAttribute('value', '0');
-        slider.setAttribute('class', 'positionSlider');
-        slider.setAttribute('id', 'positionSlider' + colorIncrement);
-        controll.append(slider);
+    //     var slider = document.createElement('input');
+    //     slider.setAttribute('type', 'range');
+    //     slider.setAttribute('min', '0');
+    //     slider.setAttribute('max', '800');
+    //     slider.setAttribute('value', '0');
+    //     slider.setAttribute('class', 'positionSlider');
+    //     slider.setAttribute('id', 'positionSlider' + colorIncrement);
+    //     controll.append(slider);
 
-        var colorchooser = document.createElement('input');
-        colorchooser.setAttribute('type', 'color');
-        colorchooser.setAttribute('value', '#ff0000');
-        colorchooser.setAttribute('class', 'colorChooser');
-        colorchooser.setAttribute('id', 'colorChooser' + colorIncrement);
-        controll.append(colorchooser);
+    //     var colorchooser = document.createElement('input');
+    //     colorchooser.setAttribute('type', 'color');
+    //     colorchooser.setAttribute('value', '#ff0000');
+    //     colorchooser.setAttribute('class', 'colorChooser');
+    //     colorchooser.setAttribute('id', 'colorChooser' + colorIncrement);
+    //     controll.append(colorchooser);
 
-        document.getElementById('colorControlls').append(controll);
+    //     document.getElementById('colorControlls').append(controll);
 
-        var indicator = document.createElement('div');
-        indicator.setAttribute('id', 'posIndicator' + colorIncrement);
-        indicator.setAttribute('class', 'posIndicator');
-        document.getElementById('colorbar').append(indicator);
+    //     var indicator = document.createElement('div');
+    //     indicator.setAttribute('id', 'posIndicator' + colorIncrement);
+    //     indicator.setAttribute('class', 'posIndicator');
+    //     document.getElementById('colorbar').append(indicator);
 
-        (function (n) {
-            slider.addEventListener('change', function (event) {
-                indicator.style.left = (event.target.value - 10) + 'px';
-                colors[n].pos = event.target.value;
-                updateColors();
-            }, false);
-        })(colorIncrement);
+    //     (function (n) {
+    //         slider.addEventListener('change', function (event) {
+    //             indicator.style.left = (event.target.value - 10) + 'px';
+    //             colors[n].pos = event.target.value;
+    //             updateColors();
+    //         }, false);
+    //     })(colorIncrement);
 
-        (function (n) {
-            colorchooser.addEventListener('change', function (event) {
-                indicator.style.background = event.target.value;
-                colors[n].color = event.target.value;
-                updateColors();
-            }, false);
-        })(colorIncrement);
+    //     (function (n) {
+    //         colorchooser.addEventListener('change', function (event) {
+    //             indicator.style.background = event.target.value;
+    //             colors[n].color = event.target.value;
+    //             updateColors();
+    //         }, false);
+    //     })(colorIncrement);
 
-        colors[colorIncrement] = { "color": "#ff0000", "pos": 0 };
+    //     colors[colorIncrement] = { "color": "#ff0000", "pos": 0 };
 
-        colorIncrement++;
-    });
+    //     colorIncrement++;
+    // });
 
     // <div id="colorControll0"></div>
 
